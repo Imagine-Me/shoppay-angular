e5e623bd84003eedb2a15bcea00409d21ccea7e3
Imagine-Me <princekizhthara@gmail.com>
2021-11-26 09:26:29 +0530

    add carousel in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppBarComponent } from './app-bar/app-bar.component';
 import { ShowDirective } from './show.directive';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppBarComponent,
-    ShowDirective
+    ShowDirective,
+    HomePageComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  slides = [
+    {
+      title: 'Trending',
+      description: 'Wear trending collections',
+      url: 'https://firebasestorage.googleapis.com/v0/b/shopay-dev.appspot.com/o/main-carousel-1.jpg?alt=media&token=957ccfd9-2db7-4651-9961-0fe57f682482'
+    }
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
