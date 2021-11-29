53db14b447168488a700fc0947f27f94b112213c
Imagine-Me <princekizhthara@gmail.com>
2021-11-29 10:04:40 +0530

    restructure folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppBarComponent } from './app-bar/app-bar.component';
+import { AppBarComponent } from './components/app-bar/app-bar.component';
 import { ShowDirective } from './show.directive';
 import { HomePageComponent } from './home-page/home-page.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { CarouselMultipleComponent } from './carousel-multiple/carousel-multiple.component';
-import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CarouselMultipleComponent } from './components/carousel-multiple/carousel-multiple.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { FooterComponent } from './footer/footer.component';
     HomePageComponent,
     CarouselComponent,
     CarouselMultipleComponent,
-    FooterComponent
+    FooterComponent,
+    SearchInputComponent
   ],
   imports: [
     BrowserModule,
