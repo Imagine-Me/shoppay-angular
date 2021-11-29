7e86f7123aadea2a9161e23079ae8cfaf4d76a4a
Imagine-Me <princekizhthara@gmail.com>
2021-11-29 12:58:07 +0530

    add list page to app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { ListPageComponent } from './pages/list-page/list-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent }
+  { path: '', component: HomePageComponent },
+  { path: 'search/:id', component: ListPageComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CarouselMultipleComponent } from './components/carousel-multiple/carous
 import { FooterComponent } from './components/footer/footer.component';
 import { SearchInputComponent } from './components/search-input/search-input.component';
 import { BottomNavigationComponent } from './components/bottom-navigation/bottom-navigation.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { BottomNavigationComponent } from './components/bottom-navigation/bottom
     CarouselMultipleComponent,
     FooterComponent,
     SearchInputComponent,
-    BottomNavigationComponent
+    BottomNavigationComponent,
+    ListPageComponent
   ],
   imports: [
     BrowserModule,
