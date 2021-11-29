9ef368930e458a98f787f64764b32b44f870e917
Imagine-Me <princekizhthara@gmail.com>
2021-11-29 12:27:57 +0530

    implement auto search option

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,4 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, startWith } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+
+export interface User {
+  name: string;
+}
+
 
 @Component({
   selector: 'app-search-input',
@@ -7,9 +15,28 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SearchInputComponent implements OnInit {
   @Input() placeHolder = "";
-  constructor() { }
+  myControl = new FormControl();
+  options: User[] = [{name: 'Mary'}, {name: 'Shelley'}, {name: 'Igor'}];
+  filteredOptions: Observable<User[]>;
+  constructor() { 
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => (typeof value === 'string' ? value : value.name)),
+      map(name => (name ? this._filter(name) : this.options.slice())),
+    );
+  }
 
   ngOnInit(): void {
+  }
+
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
   }
 
 }
